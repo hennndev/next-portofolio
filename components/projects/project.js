d97636f6219a018246a18497675c32468365c984
hennndev <hendraputra230897@gmail.com>
2021-10-30 15:18:45 +0700

    first-commmit

--- components/projects/project.js
+++ components/projects/project.js
@@ -6,7 +6,7 @@ const Project = ({imgSrc, url, title, desc}) => {
     return (
         <div className="h-cardProject shadow-lg rounded overflow-hidden bg-gray-200 text-gray-700 bg-gray-100 dark:bg-gray-800 dark:text-gray-200 relative transition duration-200 ease-in">
             <div className="h-image w-full">
-                <img src={`images/${imgSrc}`} className="w-full h-full" />   
+                <img src={`/images/${imgSrc}`} className="w-full h-full" />   
             </div>
             <div className="px-3 py-2">
                 <h3 className="mt-1 text-xl font-bold">{title}</h3>
