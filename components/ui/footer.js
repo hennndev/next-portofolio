10a9dfb4238807a7b2a0569e82965fe1fc65805d
hennndev <hendraputra230897@gmail.com>
2021-10-30 14:27:01 +0700

    first-commit

--- components/ui/footer.js
+++ components/ui/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="bg-gray-200 text-gray-700 dark:bg-gray-800 dark:text-gray-200 w-full transition duration-200 ease-in py-5">
             <div className="flex items-center justify-center">
-                ©copyright 2021 || <span className="ml-2">Designed by henndev</span>
+                copyright 2021 || <span className="ml-2">Designed by henndev</span>
             </div>
         </div>
     )
